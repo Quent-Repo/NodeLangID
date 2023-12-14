a061a0c1f28033df3f510551699ad0ae28590072
owner <grrkekisbest@gmail.com>
2023-12-13 22:41:26 -0500

    added error handle

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var langs = require('langs');
 var frank = require('franc');
+try {
+    let b = process.argv.slice(2);
+    let v = frank(b.toString());
+    let h= langs.where("2",v.toString());
+    console.log(h["name"]);   
+} catch (error) {
+    console.log("could not find anything")
+    
+}
 
-let b = process.argv.slice(2);
-let v = frank(b.toString());
-let h= langs.where("2",v.toString());
-console.log(h["name"]);
